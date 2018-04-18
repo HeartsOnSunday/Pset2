fe394e2492c9fdf45b2978f5789effaf968f3a72
Leah <leahgdg@hotmail.com>
2018-04-18 00:01:16 +0000

    vig is styled

--- vigenere/vigenere.c
+++ vigenere/vigenere.c
@@ -10,13 +10,6 @@ int main(int argC, string argV[])
     int b;
     int i;
 
-    //printf("this is the key length: %i\n", keyPhraseLength);
-
-
-    //printf("Argument Count: %i\n", argC);       //argument count
-    //printf("The Key is: %s\n", keyPhrase);
-    //printf("%i\n", keyLength);
-
     if (argC != 2)
     {
         printf("Include the you-know-what ;)\n"); //promt the user for the key
@@ -41,10 +34,8 @@ int main(int argC, string argV[])
 
     for (a = 0, b = 0; a < messageLength; a++)   //a is for Key Phrase i is for message
     {
-        //eprintf("%i\t", a);
-        int equalizeLetters = tolower(keyPhrase[b % keyPhraseLength]) - 'a';
-       //eprintf("1st equalize: %c\n", equalizeLetters);
 
+        int equalizeLetters = tolower(keyPhrase[b % keyPhraseLength]) - 'a';
         int alphabet = 26;   //use with modulo
         int upperASCII = 65; //use to adjust uppercase letters
         int lowerASCII = 97; //use to adjust lowercase letters
@@ -55,22 +46,16 @@ int main(int argC, string argV[])
             printf("%c", 'A' + (message[a] - upperASCII + equalizeLetters) % alphabet);
             //subtract ascii value and add key, use %26 to find letter, add ascii value then print
             b++;
-            //eprintf("\n");
-            //eprintf("Upper equalize: %c\n", equalizeLetters);
         }
         else if (islower(message[a]))    //if lowercase print in lowercase
         {
-            //eprintf("is alphalower\n");
             printf("%c", 'a' + (message[a] - lowerASCII + equalizeLetters) % alphabet);
             //subtract ascii value and add key, use %26 to find letter, add ascii value then print
             b++;
-            //eprintf("Lowerequalize: %c\n", equalizeLetters);
         }
         else
         {
-            //eprintf("non-alpha\n");
             printf("%c", message[a]);    //if not alphabetical, pass character through
-            //eprintf("equalize: %c\n", equalizeLetters);
         }
     }
     printf("\n");
@@ -86,10 +71,10 @@ int main(int argC, string argV[])
 
 //iterate through the phrase
 //if its a letter
-    //if its uppercase
-        //iterate through the phraseKey and return uppercase
+//if its uppercase
+//iterate through the phraseKey and return uppercase
 
-    //if its lowercase
-        //iterate through the phrasekey and return lowercase
+//if its lowercase
+//iterate through the phrasekey and return lowercase
 
-    //if its not a letter then just pass it through
+//if its not a letter then just pass it through
